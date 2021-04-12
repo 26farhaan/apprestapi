bea755f0fd2c237b662070c8a7b819049305f9d6
farhan <farhan.21144@gmail.com>
2021-04-13 06:10:53 +0700

    get data mahasiswa

--- controller.js
+++ controller.js
@@ -6,3 +6,13 @@ var connection = require('./koneksi');
 exports.index = function (req, res) {
     response.ok("Aplikasi REST API berjalan", res)
 };
+
+exports.allMahasiswa = function (req, res) {
+    connection.query('SELECT * FROM mahasiswa', function (error, rows, fields) {
+        if(error){
+            connection.log(error)
+        } else {
+            response.ok(rows, res)
+        }
+    })
+}
