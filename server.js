0091ddc7cdcd089d0688116d9602ae5c56927abd
farhan <farhan.21144@gmail.com>
2021-04-13 06:03:25 +0700

    penambahan routes

--- server.js
+++ server.js
@@ -5,6 +5,11 @@ const app = express();
 //parse applicarion/json
 app.use(bodyParser.urlencoded({extended : true}));
 app.use(bodyParser.json());
+
+//routes
+var routes = require('./router')
+routes(app)
+
 app.listen(3000, () => {
     console.log(`Server started on port 3000`)
 })
